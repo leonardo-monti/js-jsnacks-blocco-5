db7bb1cd35ec0df22ae84767104dcb8615095c8e
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-21 15:54:39 +0100

    end snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+const newArray = posts.map(post => {
+  return post.author
+})
+
+console.log(newArray)
